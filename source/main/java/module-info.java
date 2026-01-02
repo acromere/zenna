69e257144be66bf7efa02bd24db7fca62a7c7d6c
Mark Soderquist <mark@soderquist.net>
2026-01-01 18:46:15 -0700

    Update module exports and Maven argLine configuration for `com.acromere.zenna.icon`.

--- source/main/java/module-info.java
+++ source/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.acromere.zenna {
 	requires com.acromere.zerra;
 	requires javafx.graphics;
 	requires java.base;
-	requires java.logging;
+	//requires java.logging;
 
 	exports com.acromere.zenna.icon;
 }
